4524aa30a75acd165a51bc129639526e5156349d
Remco Tolsma <info@remcotolsma.nl>
2017-05-08 14:07:19 +0200

    Init commit.

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<html dir="ltr" lang="nl">
+	<head>
+		<meta charset="UTF-8" />
+
+		<title>Huisstijl | Pronamic</title>
+
+		<link rel="stylesheet" href="index.css" media="all" />
+	</head>
+
+	<body>
+		<header>
+			<h1>
+				<a href="">
+					Huisstijl | Pronamic
+				</a>
+			</h1>
+		</header>
+
+		<div>
+			<h2>RGB</h2>
+
+			<ul>
+				<li>
+					<figure>
+						<img src="rgb/logo-without-text/logo-16x16.png" alt="" class="transparent" />
+
+						<figcaption>Logo 16x16</figcaption>
+					</figure>
+				</li>
+				<li>
+					<figure>
+						<img src="rgb/logo-without-text/logo-57x57.png" alt="" class="transparent" />
+
+						<figcaption>Logo 57x57</figcaption>
+					</figure>
+				</li>
+				<li>
+					<figure>
+						<img src="rgb/logo-without-text/logo-512x512.png" alt="" class="transparent" />
+
+						<figcaption>Logo 512x512</figcaption>
+					</figure>
+				</li>
+			</ul>
+		</div>
+
+		<footer>
+			<p>
+				&copy; 2005-2010 Pronamic
+			</p>
+		</footer>
+	</body>
+</html>
